98eb47d0b0c3d11d9f0c0873fb361d2ce675d408
Suprath PS <suprathps@gmail.com>
2025-08-30 01:18:08 +0530

    workflow test 3

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import subprocess
 import uvicorn
 
 app = FastAPI(title="Vulnerable Sample App")
-# A small change to trigger the workflow 3
+# A small change to trigger the workflow 4
 
 # A safe, root endpoint
 @app.get("/")
